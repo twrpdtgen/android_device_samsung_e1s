83126775082b8e720181c12a7af42a6356651224
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-10-07 12:29:28 +0200

    e1s: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-S921B Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_e1s.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_e1s-user \
+    omni_e1s-userdebug \
+    omni_e1s-eng
